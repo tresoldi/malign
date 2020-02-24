83c9f056a52fc0373b8fe11caedaff6ea2869e5b
Tiago Tresoldi <tresoldi@shh.mpg.de>
2020-02-24 17:40:04 +0100

    Import previous scorer code.

--- tests/test_malign.py
+++ tests/test_malign.py
@@ -37,6 +37,57 @@ class TestMalign(unittest.TestCase):
         assert tuple(alm_b) == ("f", "a", "t", "-", "a")
         assert math.isclose(score, 0.0)
 
+    # TODO: test class only for multiple alignment?
+    def test_fill_scorer(self):
+        """
+        Test scorer filling.
+        """
+
+        # TODO: test from zero first
+        REF_SCORER = {
+            ("A", "A"): 10,
+            ("A", "G"): -1,
+            ("A", "C"): -3,
+            ("A", "T"): -4,
+            ("G", "A"): -1,
+            ("G", "G"): 7,
+            ("G", "C"): -5,
+            ("G", "T"): -3,
+            ("C", "A"): -3,
+            ("C", "G"): -5,
+            ("C", "C"): 9,
+            ("C", "T"): 0,
+            ("T", "A"): -4,
+            ("T", "G"): -3,
+            ("T", "C"): 0,
+            ("T", "T"): 8,
+            ("A", "-"): -5,
+            ("G", "-"): -5,
+            ("C", "-"): -5,
+            ("T", "-"): -5,
+            ("-", "A"): -5,
+            ("-", "G"): -5,
+            ("-", "C"): -5,
+            ("-", "T"): -5,
+        }
+
+        scorer = malign.fill_scorer("AGCT", "AGCT", REF_SCORER)
+        assert math.isclose(scorer["A", "A"], REF_SCORER["A", "A"])
+        assert math.isclose(scorer["A", "T"], REF_SCORER["A", "T"])
+        assert math.isclose(scorer["A", "-"], REF_SCORER["A", "-"])
+        assert scorer["-", "-"] == 0.0
+
+        # Remove some entries and check computation from mean values
+        del scorer["A", "A"]
+        del scorer["A", "T"]
+        del scorer["A", "-"]
+
+        scorer = malign.fill_scorer("AGCT", "AGCT", scorer)
+        assert math.isclose(scorer["A", "A"], 6.0)
+        assert math.isclose(scorer["A", "T"], -2.545454545454)
+        assert math.isclose(scorer["A", "-"], -5.0)
+        assert scorer["-", "-"] == 0.0
+
 
 if __name__ == "__main__":
     # Explicitly creating and running a test suite allows to profile it
