b3d8024c2f54286480d40013713b61860d997716
Tiago Tresoldi <tresoldi@shh.mpg.de>
2020-02-25 10:16:52 +0100

    Implement basic command-line functionality

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ Code and data for the library on Multiple ALIGNments
 ## TODO
 
 - Setup readthedocs
+- Sort in consistent and reproducible way all alignments, even when the
+  score is the same
+- Deal with conflicting package versions due to lingpy
--- malign/__main__.py
+++ malign/__main__.py
@@ -12,6 +12,34 @@ import argparse
 # Import our library
 import malign
 
+# TODO: Remove temporary DNA scorer holder in future versions
+DNA_SCORER = {
+    ("A", "A"): 10,
+    ("A", "G"): -1,
+    ("A", "C"): -3,
+    ("A", "T"): -4,
+    ("G", "A"): -1,
+    ("G", "G"): 7,
+    ("G", "C"): -5,
+    ("G", "T"): -3,
+    ("C", "A"): -3,
+    ("C", "G"): -5,
+    ("C", "C"): 9,
+    ("C", "T"): 0,
+    ("T", "A"): -4,
+    ("T", "G"): -3,
+    ("T", "C"): 0,
+    ("T", "T"): 8,
+    ("A", "-"): -5,
+    ("G", "-"): -5,
+    ("C", "-"): -5,
+    ("T", "-"): -5,
+    ("-", "A"): -5,
+    ("-", "G"): -5,
+    ("-", "C"): -5,
+    ("-", "T"): -5,
+}
+
 
 def parse_arguments():
     """
@@ -24,6 +52,17 @@ def parse_arguments():
     )
     parser.add_argument(
         "seq_b", type=str, help="The second string to be aligned"
+    )
+    parser.add_argument(
+        "--dna",
+        action="store_true",
+        help="Whether to use the standard DNA scorer (default: uniform scorer)",
+    )
+    parser.add_argument(
+        "-k",
+        type=int,
+        help="Number of k-best alignments to output (default: 4)",
+        default=4,
     )
     args = parser.parse_args()
 
@@ -38,12 +77,24 @@ def main():
     # Parse command-line arguments
     args = parse_arguments()
 
-    # Get alignments
-    alm_a, alm_b = malign.align(args.seq_a, args.seq_b)
+    # Run alignments
+    dna_seq1 = [base for base in args.seq_a]
+    dna_seq2 = [base for base in args.seq_b]
+    if args.dna:
+        scorer = malign.fill_scorer("ACGT", "ACGT", DNA_SCORER)
+    else:
+        scorer = malign.fill_scorer("ACGT", "ACGT")
+    graph = malign.compute_graph(dna_seq1, dna_seq2, scorer)
 
-    # Output alignments
-    print("A: [%s]" % " ".join(alm_a))
-    print("B: [%s]" % " ".join(alm_b))
+    dest = "%i:%i" % (len(dna_seq1), len(dna_seq2))
+    aligns = malign.get_aligns(graph, ("0:0", dest), dna_seq1, dna_seq2, args.k)
+
+    # Output
+    for idx, align in enumerate(aligns):
+        print("Alignment #%i (score: %.2f)" % (idx, align[1]))
+        print(" ".join(align[0][0]))
+        print(" ".join(align[0][1]))
+        print()
 
 
 if __name__ == "__main__":
