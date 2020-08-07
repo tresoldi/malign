3ea709a7a626b7dbab3c37f97b0805158e3e099f
Tiago Tresoldi <tresoldi@shh.mpg.de>
2020-08-07 14:48:14 +0200

    Restructure project files

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
-import pathlib
-from setuptools import setup
+from pathlib import Path
+import glob
+from setuptools import setup, find_packages
 
 # The directory containing this file
-LOCAL_PATH = pathlib.Path(__file__).parent
+LOCAL_PATH = Path(__file__).parent
 
 # The text of the README file
 README_FILE = (LOCAL_PATH / "README.rst").read_text()
+
+# Build (recursive) list of resource files
+resource_files = []
+for directory in glob.glob("resources/*/"):
+    files = glob.glob(directory + "*")
+    resource_files.append((directory, files))
 
 # Load requirements, so they are listed in a single place
 with open("requirements.txt") as fp:
@@ -21,6 +28,7 @@ setup(
         "Programming Language :: Python :: 3",
         "Topic :: Software Development :: Libraries",
     ],
+    data_files=[("docs", glob.glob("docs/*"))] + resource_files,
     description="Library for multiple asymmetric alignments on different alphabets",
     entry_points={"console_scripts": ["malign=malign.__main__:main"]},
     include_package_data=True,
@@ -30,7 +38,8 @@ setup(
     long_description_content_type="text/x-rst",
     long_description=README_FILE,
     name="malign",
-    packages=["malign", "resources", "docs"],
+    packages=find_packages(where="src"),  # ["malign", "resources", "docs"],
+    package_dir={"": "src"},  # , "resources":"..", "docs":".."},
     project_urls={"Documentation": "https://malign.readthedocs.io"},
     test_suite="tests",
     tests_require=[],
